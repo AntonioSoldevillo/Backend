1a2d279df4dd7cebed6bfab104cb5f2422bde342
oninchan <soldevillo.antonio09@gmail.com>
2025-04-03 19:16:36 +0800

    newwwwwww

--- todolist/views.py
+++ todolist/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from .models import ToDoItem
 from .serializers import ToDoItemSerializer
 
+
 class ToDoItemViewSet(viewsets.ModelViewSet):
     queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemSerializer
+
+
